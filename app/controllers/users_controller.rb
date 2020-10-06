9ad16b0a8c30fe58a63f80ebfc3fe7438d919c00
Jonathan East <jonathan_east@ymail.com>
2020-10-06 16:33:52 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
-  def new
-
-  end
 
   def create
+    User.create(user_params)
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password_digest)
   end
 end
