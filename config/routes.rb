1cc0aef8f6b27b251a03321f620e8e39022e58f8
Jonathan East <jonathan_east@ymail.com>
2020-10-06 16:37:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
 end
