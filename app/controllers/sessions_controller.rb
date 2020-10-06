d4fe6734bdd3e7645d40f811b3c29185694aea75
Jonathan East <jonathan_east@ymail.com>
2020-10-06 16:44:01 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,17 @@
 class SessionsController < ApplicationController
 
+  def new
+
+  end
+
   def create
     @user = User.find_by(name: params[:name])
     return head(:forbidden) unless @user.authenticate(params[:password_digest])
     session[:user_id] = @user.id
+  end
+
+  def destroy
+    session.delete :user_id
   end
 
 end
