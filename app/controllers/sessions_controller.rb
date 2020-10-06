e62f4ddda3469a94ff84d42eae908aa0ef616ce1
Jonathan East <jonathan_east@ymail.com>
2020-10-06 16:46:43 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:user][:name])
     user = user.try(:authenticate, params[:user][:password])
     session[:user_id] = user.id
+    @user = user
   end
 
   def destroy
